5586839f47b5e01196d54e5f3340ad57e05af115
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-07-30 17:33:54 +0300

    update run.rb

--- run.rb
+++ run.rb
@@ -1,20 +1,24 @@
 #!/usr/bin/env ruby
 
-require "socket"
-require "yaml"
-require "active_support/all"
+require 'socket'
+require 'yaml'
+require 'active_support/all'
 
+require_relative 'src/server'
 
-require_relative "src/server"
+$config = YAML.load_file File.join(__dir__, 'config.yml')
 
-$config = YAML.load_file File.join(__dir__, "config.yml")
-
-port = ENV.fetch("PORT", $config['port']).to_i
+port = ENV.fetch('PORT', $config['port']).to_i
 ip = $config['ip']
+dir = '/'
 
-if ARGV.length != 1
-  abort "One argument is needed";
+if ARGV.empty?
+  dir = '.'
+elsif ARGV.length > 1
+  abort 'One argument is needed'
+else
+  dir = ARGV[0]
 end
 
-server = Server.new(ARGV[0], ip, port)
+server = Server.new(dir, ip, port)
 server.run
